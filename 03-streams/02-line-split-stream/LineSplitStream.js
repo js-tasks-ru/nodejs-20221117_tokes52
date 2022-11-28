9f11dec6844fe4b4e899aed8e2a035148783082c
Steph Trofimov <stephan@ecomsolutions.net>
2022-11-28 22:03:04 +0100

    add solution for 3-module 2-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,30 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._remainingData = null;
   }
 
   _transform(chunk, encoding, callback) {
+    const lines = chunk.toString().split(os.EOL);
+
+    if (this._remainingData) {
+      lines[0] = this._remainingData + lines[0];
+    }
+
+    [this._remainingData] = lines.splice(lines.length - 1, 1);
+
+    lines.forEach((line) => {
+      this.push(line);
+    });
+
+    callback();
   }
 
   _flush(callback) {
+    if (this._remainingData) {
+      this.push(this._remainingData);
+    }
+    callback();
   }
 }
 
