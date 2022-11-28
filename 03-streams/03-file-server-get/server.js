8fe85de507a56ebdaa9a584edb53a6ab61756a38
Steph Trofimov <stephan@ecomsolutions.net>
2022-11-29 00:53:39 +0100

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,23 +1,45 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = url.pathname.slice(1);
+  try {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+    switch (req.method) {
+      case 'GET': {
+        const paths = pathname.split('/');
+        if (paths.length > 1) {
+          res.statusCode = 400;
+          res.end('No nested directories supported');
+          return;
+        }
 
-  switch (req.method) {
-    case 'GET':
+        const filepath = path.join(__dirname, 'files', pathname);
+        const read = fs.createReadStream(filepath);
+        read.on('error', (e) => {
+          if (e.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('Not found');
+            return;
+          }
+          res.statusCode = 500;
+          res.end(e.message || 'Internal server error');
+        });
 
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+        read.pipe(res);
+        break;
+      }
+      default:
+        res.statusCode = 501;
+        res.end('Not implemented');
+    }
+  } catch (e) {
+    res.statusCode = 500;
+    res.end(e.message || 'Internal server error');
   }
 });
 
