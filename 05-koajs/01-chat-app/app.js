a8131d78e2bc25d9bd810f5a6c72f0c2f184245a
Steph Trofimov <stephan@ecomsolutions.net>
2022-12-17 18:20:48 +0100

    add solution for 5-module 1-task

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,28 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+const subscribers = new Set();
+
+router.get('/subscribe', async (ctx) => {
+  const message = await new Promise((resolve) => {
+    subscribers.add(resolve);
+  });
+
+  ctx.status = 200;
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+
+  if (message) {
+    subscribers.forEach((subscriber) => {
+      subscriber(message);
+    });
+    subscribers.clear();
+  }
+
+  ctx.body = 'OK';
 });
 
 app.use(router.routes());
